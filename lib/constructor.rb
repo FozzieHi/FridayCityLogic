987725e10e761848f2241e89fdc822c21d58b32e
George <fozzziedoesmc@gmail.com>
2019-03-17 07:24:12 +0000

    Update constructor.rb

--- lib/constructor.rb
+++ lib/constructor.rb
@@ -1,5 +1,5 @@
 class Constructor
-  attr_accessor :cost, :name, :averagepointsperrace, :past_avg_points
+  attr_accessor :cost, :name, :averagepointsperrace
 
   def initialize(constructordata, name)
     @name = name
@@ -11,10 +11,8 @@ class Constructor
   def populate_constructor(constructordata)
     @cost = constructordata['cost']
     points = constructordata['points_history'].inject(0){|sum,x| sum + x }
-    past_points = constructordata['2018_points_history'].inject(0) { |sum, x| sum + x }
     race_count = constructordata['points_history'].length
     @averagepointsperrace = race_count == 0 ? 0 : points / race_count
-    @past_avg_points = past_points / 21
   end
 
 end
